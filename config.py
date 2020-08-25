18ca6390412928772bdc0c0ef92866799f974f94
Quastrado <you@example.com>
2020-08-25 23:12:56 +0300

    config file

---
+++ config.py
@@ -0,0 +1,2 @@
+DOLLAR_RUB = 'https://www.google.com/search?q=%D0%B4%D0%BE%D0%BB%D0%BB%D0%B0%D1%80+%D0%BA+%D1%80%D1%83%D0%B1%D0%BB%D1%8E&oq=%D0%B4%D0%BE%D0%BB%D0%BB%D0%B0%D1%80+%D0%BA+%D1%80%D1%83%D0%B1%D0%BB%D1%8E&aqs=chrome..69i57j0l7.4459j1j15&sourceid=chrome&ie=UTF-8'
+HEADERS = {'User-Agent': 'your user-agent'}
